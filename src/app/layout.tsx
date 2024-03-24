fa9810d58abd68900534ed47ec2b32c282f48266
feby-tw <feby230201@gmail.com>
2024-03-24 18:57:24 +0700

    eight update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,17 @@
 import React, { ReactNode } from 'react';
 import Header from '../components/header';
-import Footer from '../components/footer';
 import './globals.css';
 
 interface LayoutProps {
   children: ReactNode;
   showHeader?: boolean;
-  showFooter?: boolean;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, showHeader = true, showFooter = true }) => {
+const Layout: React.FC<LayoutProps> = ({ children, showHeader = true }) => {
   return (
     <div className='min-h-screen flex flex-col'>
       {showHeader && <Header />} {}
       <main className='flex-grow'>{children}</main>
-      {showFooter && <Footer />} {}
     </div>
   );
 };
