f4421b0cede8ce9c039baf23a230bdddd21f4a78
feby-tw <feby230201@gmail.com>
2024-03-25 15:55:43 +0700

    final update

--- src/components/teams/explore-job.tsx
+++ src/components/teams/explore-job.tsx
@@ -7,7 +7,7 @@ const ExploreJob = () => {
     <div className='bg-red-600 ml-40 mr-40 pt-20 pb-20 text-center tracking-widest rounded-3xl'>
       <p className='text-gray-300 text-lg'>APPLY FOR JOBS NOW</p>
       <h1 className='text-4xl mb-8'>SHARE YOUR PASSION<br />WITH US</h1>
-      <Link href='https://career.eigeradventure.com/jobs' className='inline-flex justify-center items-center rounded-full bg-black text-white p-4 pl-12 pr-12 tracking-widest hover:font-bold'>
+      <Link href='https://career.eigeradventure.com/jobs' target='_blank' className='inline-flex justify-center items-center rounded-full bg-black text-white p-4 pl-12 pr-12 tracking-widest hover:font-bold'>
         <span className='text-center'>EXPLORE JOBS </span><ArrowRightIcon className='h-5 ml-2' />
       </Link>
     </div>
